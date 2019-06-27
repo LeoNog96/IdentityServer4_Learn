190ce610ec51df741186daf61b3f3071e0c00d31
Leonardo Nogueira da Silva <leonardo@digitaldoc.com.br>
2019-06-27 11:58:56 -0300

    O AddTestUsersmétodo de extensão faz algumas coisas sob o capô
    adiciona suporte para a concessão de senha do proprietário do recurso
    adiciona suporte a serviços relacionados ao usuário normalmente usados
    ​​por uma interface de usuário de login (usaremos isso no
    próximo início rápido)
    adiciona suporte para um serviço de perfil baseado nos usuários de teste
    (você aprenderá mais sobre isso no próximo início rápido)

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,9 +1,6 @@
-﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
-
-using IdentityServer4.Models;
+﻿using IdentityServer4.Models;
 using System.Collections.Generic;
+using IdentityServer4.Test;
 
 namespace IdentityServer
 {
@@ -23,7 +20,7 @@ namespace IdentityServer
                 new ApiResource("api1","My API")
             };
         }
-
+        
         public static IEnumerable<Client> GetClients()
         {
             return new List<Client>
@@ -43,6 +40,25 @@ namespace IdentityServer
 
                     // scopes that client has access to
                     AllowedScopes = { "api1" }
+                }
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "password"
                 }
             };
         }
